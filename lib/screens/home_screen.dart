68a2f4f37704915d6e0f8068cba317237362564d
FeanorD <meleshkodim2000@gmail.com>
2021-03-03 20:53:37 +0200

    Create function for handling text color

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,33 +7,46 @@ class HomeScreen extends StatefulWidget {
   HomeScreenState createState() => HomeScreenState();
 }
 
-//HomeScreenState is not private only for widget testing in test/screens/home_screen_test.dart
+//HomeScreenState is not private because of widget testing
+//in test/screens/home_screen_test.dart
 class HomeScreenState extends State<HomeScreen> {
-  Color _currentColor;
+  Color _currentBackgroundColor;
+  Color _textColor;
 
   @override
   void initState() {
     super.initState();
-    _currentColor = _getRandomColor();
+    _currentBackgroundColor = _getRandomColor();
+    _setTextColor();
   }
 
-  ///Getter [currentColor] only for widget testing
-  Color get currentColor => _currentColor;
+  //Getter for _currentColor. Is needed only for widget testing
+  Color get currentColor => _currentBackgroundColor;
 
   void _changeColor() {
     setState(() {
-      _currentColor = _getRandomColor();
+      _currentBackgroundColor = _getRandomColor();
+      _setTextColor();
     });
   }
 
   Color _getRandomColor() {
     Random random = Random();
     return Color.fromARGB(
-      random.nextInt(256),
+      255,
       random.nextInt(256),
       random.nextInt(256),
       random.nextInt(256),
     );
+  }
+
+  // Sets the text color to black or white color depends on the lightness
+  // of the background color
+  void _setTextColor() {
+    final currentBgColorHSL = HSLColor.fromColor(_currentBackgroundColor);
+    _textColor = currentBgColorHSL.lightness > 0.6
+        ? Colors.black
+        : Colors.white;
   }
 
   @override
@@ -41,19 +54,23 @@ class HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-          color: _currentColor,
+          color: _currentBackgroundColor,
         ),
-        //It also could be GestureDetector here instead of InkWell,
+        //It also could be the GestureDetector here instead of the InkWell,
         //but I decided to use it because of its nice ripple animation
-        child: InkWell(
-          onTap: _changeColor,
-          child: SafeArea(
-            child: Center(
-              child: Text(
-                'Hey there',
-                style: TextStyle(
-                  fontSize: 36,
-                  fontWeight: FontWeight.w600,
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: _changeColor,
+            child: SafeArea(
+              child: Center(
+                child: Text(
+                  'Hey there',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.w600,
+                  color: _textColor,
+                  ),
                 ),
               ),
             ),
